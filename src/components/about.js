ab5c3448dc1ec03032588074c126eda07e377cff
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-07 15:11:54 -0800

    Create about.js

---
+++ src/components/about.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export const About = () => {
+  return (
+    <div>
+      <h2>About DreamBuildGarage.llc</h2>
+      <p>
+        Need regular maintenance? Let us know what service you require” - “Got
+        car issues but not exactly sure what it could be? Let us diagnose the
+        problem.” , -“want to take your car to the next level? We offer
+        professional parts installation, as well as engine/transmission tuning
+        to get the most out those aftermarket parts” -”Want to squeeze more
+        power out of your car but not sure where to start? Let us guide you on
+        your build, we'll make sure to get you exactly what you need.” -”
+        Questions on our power packages? Let us know how we could clarify any
+        doubts and further assist you”
+      </p>
+    </div>
+  )
+}
