b776cce5fa66ea2bb90b883622d1656c63d59af0
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-07 15:14:38 -0800

    landing page reorganized

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { AppointmentForm, Carousel, DropDownMenu } from "../components/antd"
-
-import { useState } from "react"
+import { Button } from "antd"
+import { Carousel } from "../components/antd"
+import { Services } from "../components/services"
+import { About } from "../components/about"
 
 const IndexPage = () => {
-  const [finish, setFinish] = useState(false)
   return (
     <Layout>
       <Seo title="Home" />
-      <Carousel/>
-      {finish ? (
-        <div>
-          <h1>thank you for booking an appointment.</h1>
-          <p>you'll be receiving a confirmation shortly</p>
-        </div>
-      ) : (
-        <AppointmentForm setFinish={setFinish} />
-      )}
-
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        width={300}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt="A Gatsby astronaut"
-        style={{ marginBottom: `1.45rem` }}
-      />
-      <p>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-        <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-        <Link to="/using-dsg">Go to "Using DSG"</Link>
-      </p>
+      <Carousel />
+      <Link to="/page-2/">
+        <Button type="primary">Get quote</Button>
+      </Link>
+      <About />
+      <Services />
     </Layout>
   )
 }
