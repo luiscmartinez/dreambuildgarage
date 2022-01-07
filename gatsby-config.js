ae53ba36cf50a36fb26b32ddfbc7e1890818cbc2
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-07 02:25:56 -0800

    rename site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Dream Build Garage`,
+    title: `DreamBuildGarage.llc`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
