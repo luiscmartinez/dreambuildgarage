f2460ffcf0833132c683b14aa04cf8bbbea8be09
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-07 05:16:29 -0800

    importing carousel images
    using temp images for carousel preview

--- src/components/antd/carousel/index.js
+++ src/components/antd/carousel/index.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Carousel as AntCarousel } from "antd"
 
 const contentStyle = {
@@ -8,23 +9,48 @@ const contentStyle = {
   lineHeight: "160px",
   textAlign: "center",
   background: "#364d79",
+  border: "3px solid black",
+  overflow: "hidden",
 }
 
 export function Carousel() {
+  const imageData = useStaticQuery(graphql`
+    query LandingCarouselQuery {
+      allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+        edges {
+          node {
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: BLURRED
+                transformOptions: { cropFocus: CENTER }
+              )
+            }
+          }
+        }
+      }
+    }
+  `)
+  const carouselImages = imageData.allFile.edges.map(edge =>
+    getImage(edge.node)
+  )
+
   return (
     <AntCarousel autoplay>
-    <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </AntCarousel>
+      <div>
+        <h3 style={contentStyle}>
+          <GatsbyImage image={carouselImages[3]} />
+        </h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>
+          <GatsbyImage image={carouselImages[2]} />
+        </h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>
+          <GatsbyImage image={carouselImages[5]} />
+        </h3>
+      </div>
+    </AntCarousel>
   )
 }
