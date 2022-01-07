2257565668e8ca9d429da24bac51bc8ad583e943
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-07 15:14:11 -0800

    appointment form gets it's own page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,25 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { useState } from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { AppointmentForm } from "../components/antd"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const [finish, setFinish] = useState(false)
 
+  return (
+    <Layout>
+      <Seo title="book an appointment" />
+      {finish ? (
+        <div>
+          <h1>thank you for booking an appointment.</h1>
+          <p>you'll be receiving a confirmation shortly</p>
+        </div>
+      ) : (
+        <AppointmentForm setFinish={setFinish} />
+      )}
+    </Layout>
+  )
+}
 export default SecondPage
