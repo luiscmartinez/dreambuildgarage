b39cad9271ec37414306b4751d9e0654481ef0ee
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-12 00:44:28 -0800

    updating dropdown menu
    Adding icons to menu items
    + adding some services as submenu items

--- src/components/antd/dropdown-menu/index.js
+++ src/components/antd/dropdown-menu/index.js
@@ -1,20 +1,48 @@
 import React from "react"
 import { Menu, Dropdown } from "antd"
 import { GiHamburgerMenu } from "@react-icons/all-files/gi/GiHamburgerMenu"
+import { AiFillYoutube } from "@react-icons/all-files/ai/AiFillYoutube"
+import { AiFillInstagram } from "@react-icons/all-files/ai/AiFillInstagram"
+import { GrGallery } from "@react-icons/all-files/gr/GrGallery"
+import { FaTools } from "@react-icons/all-files/fa/FaTools"
+import { AiFillCar } from "@react-icons/all-files/ai/AiFillCar"
+import { MailOutlined, SettingOutlined } from "@ant-design/icons"
 import { Link } from "gatsby"
+import { InstagramBtn } from "../../buttons"
+
+const { SubMenu } = Menu
 
 const menu = (
-  <Menu>
-    <Menu.Item key="0">
+  <Menu mode="inline">
+    <Menu.Item key="0" icon={<FaTools />}>
       <Link to="/page-2/">book appointment</Link>
     </Menu.Item>
-    <Menu.Item key="1">
+    <Menu.Divider />
+    <Menu.Item key="1" icon={<AiFillCar />}>
       <a>about us</a>
     </Menu.Item>
     <Menu.Divider />
-    <Menu.Item key="2">gallery</Menu.Item>
-    <Menu.Item key="3">youtube</Menu.Item>
-    <Menu.Item key="4">instagram</Menu.Item>
+    <Menu.Item key="2" icon={<GrGallery />}>
+      gallery(coming soon)
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3" icon={<AiFillYoutube />}>
+      youtube(coming soon)
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="4" icon={<AiFillInstagram />}>
+      <InstagramBtn />
+    </Menu.Item>
+    <Menu.Divider />
+    <SubMenu key="sub1" icon={<SettingOutlined />} title="Services">
+      <Menu.Item key="5">Oil change</Menu.Item>
+      <Menu.Item key="6">Brake & rotors</Menu.Item>
+      <Menu.Item key="7">Turbo kits/ turbo swaps/ upgraded turbos</Menu.Item>
+      <Menu.Item key="8">
+        downpipes/chargepipes / cold air intake install
+      </Menu.Item>
+      <Menu.Item key="9">Turbo kits/ turbo swaps/ upgraded turbos</Menu.Item>
+    </SubMenu>
   </Menu>
 )
 
