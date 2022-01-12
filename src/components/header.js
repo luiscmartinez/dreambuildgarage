b52c81d8aba47b8faeca5d95aed2e32ee9188085
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-12 00:40:57 -0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import {DropDownMenu} from '../components/antd'
+import { DropDownMenu } from "../components/antd"
 
 const Header = ({ siteTitle }) => (
   <header
