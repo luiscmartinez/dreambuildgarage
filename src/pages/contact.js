e8409dcd2b20f64ab139f404d15ad9db62cbaa82
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-25 09:58:24 -0800

    handling onLoad iframe

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,26 +2,30 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { AppointmentForm } from "../components/antd"
+import { IframeLoader } from "../components/loaders"
 
 const ContactUsPage = () => {
+  const [isLoading, setLoading] = React.useState(true)
+  const handleLoad = () => {
+    setLoading(false)
+  }
   return (
     <Layout>
       <Seo title="Contact DreamBuildGarage" />
       <h1>Contact Us</h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+      <div>
+        {isLoading ? (
+          <div>
+            <IframeLoader />
+          </div>
+        ) : null}
         <iframe
           width="100%"
           height="250"
           frameborder="0"
           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCauUlyv92zgIER31WX5LaoBgZvo_rNSWg&q=7542+Warner+Ave,Huntington+Beach,CA"
           allowfullscreen
+          onLoad={handleLoad}
         />
         <h2>999-999-9999</h2>
         <AppointmentForm />
