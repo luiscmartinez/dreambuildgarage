f479a648521951d7d0a0bfc5c8b33f4d6044726b
Luis Martinez <mrluismartinezzz@gmail.com>
2022-01-25 07:30:49 -0800

    removed logic into AppointmentForm component

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,24 +1,14 @@
 import * as React from "react"
-import { useState } from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { AppointmentForm } from "../components/antd"
 
 const SecondPage = () => {
-  const [finish, setFinish] = useState(false)
-
   return (
     <Layout>
       <Seo title="book an appointment w DBG" />
-      {finish ? (
-        <div>
-          <h1>thank you for booking an appointment.</h1>
-          <p>you'll be receiving a confirmation shortly</p>
-        </div>
-      ) : (
-        <AppointmentForm setFinish={setFinish} />
-      )}
+      <AppointmentForm />
     </Layout>
   )
 }
