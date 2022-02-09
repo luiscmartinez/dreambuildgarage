0f7597f82380c5357a5f786b842c61addb07fca4
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-08 18:27:15 -0800

    Update about.js
    new about description & list of services

--- src/components/about.js
+++ src/components/about.js
@@ -3,17 +3,31 @@ import React from "react"
 export const About = () => {
   return (
     <div>
-      <h2>About DreamBuildGarage.llc</h2>
+      <h2>Committed to Quality at Every Step</h2>
       <p>
-        Need regular maintenance? Let us know what service you require” - “Got
-        car issues but not exactly sure what it could be? Let us diagnose the
-        problem.” , -“want to take your car to the next level? We offer
-        professional parts installation, as well as engine/transmission tuning
-        to get the most out those aftermarket parts” -”Want to squeeze more
-        power out of your car but not sure where to start? Let us guide you on
-        your build, we'll make sure to get you exactly what you need.” -”
-        Questions on our power packages? Let us know how we could clarify any
-        doubts and further assist you”
+        Got car issues but not exactly sure what it could be? Let us diagnose
+        the problem.
+        <br />
+        When you visit Dream Build Garage, it’s important to us that your
+        experience be as smooth & convenient as possible. To better serve you &
+        your car, we keep our shop fully equipped with the latest factory-grade
+        tools & equipment available. This means we are ASE certified mechanics
+        that can handle everything your car needs, including:
+      </p>
+      <ul className="ul-services">
+        <li>A/C Service & Repairs</li>
+        <li>Engine Repairs</li>
+        <li>Brake Repairs</li>
+        <li>Oil Change Services</li>
+        <li>Check Engine Light Diagnostics</li>
+        <li>Suspension Services & Repairs</li>
+        <li>Clutch Repairs</li>
+      </ul>
+      <p>
+        With decades of experience working on European vehicles, we understands
+        the importance of quality when it comes to service, so we’ll never cut
+        corners or rush a job. You can count on us to work with you through
+        every step of the maintenance process.
       </p>
     </div>
   )
