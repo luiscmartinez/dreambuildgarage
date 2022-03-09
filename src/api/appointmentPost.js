a45e9f2dbf18eec7ed5de154071969ec3f3643f5
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-09 10:56:04 -0800

    Update appointmentPost.js

--- src/api/appointmentPost.js
+++ src/api/appointmentPost.js
@@ -1,8 +1,8 @@
-import axios from "./index"
+import axios from "axios"
 
 export default function createAppointment(fields) {
   const { email, phone, body } = fields
-  return axios.post("/appointments", {
+  return axios.post("https://dreambuild.herokuapp.com/api/appointments", {
     email,
     phone,
     body,
