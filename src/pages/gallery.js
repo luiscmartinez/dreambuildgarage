62cc5ff0aed79bedb174145bbf48ea133f03ac4d
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-12 00:36:45 -0800

    Gallery Component makes API call

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,30 +1,54 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Gallery from "react-grid-gallery"
-
-const IMAGES = [
-  {
-    src: "https://scontent-lax3-2.cdninstagram.com/v/t51.29350-15/106457106_2323330747962689_2859253279724209238_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=PKjY-W7koNEAX-rLTwu&_nc_ht=scontent-lax3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AT8aU0D6yaChGTMfx5CNj42KN2tpxP6FM-TaIiT0oB4gog&oe=6200A039",
-    thumbnail:
-      "https://scontent-lax3-2.cdninstagram.com/v/t51.29350-15/106457106_2323330747962689_2859253279724209238_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=PKjY-W7koNEAX-rLTwu&_nc_ht=scontent-lax3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AT8aU0D6yaChGTMfx5CNj42KN2tpxP6FM-TaIiT0oB4gog&oe=6200A039",
-    caption: "For God blessed me with you, you make me feel brand new ;*",
-  },
-  {
-    src: "https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/10520401_294625454074163_1006333421_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=tPzi1KE5TLoAX8xTisU&_nc_ht=scontent-lax3-1.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AT-pq7qHZ1DNA8aFx7m7XYNkwbJ8ZwiqnkjHIJChOZRbww&oe=6200DC32",
-    thumbnail:
-      "https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/10520401_294625454074163_1006333421_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=tPzi1KE5TLoAX8xTisU&_nc_ht=scontent-lax3-1.cdninstagram.com&edm=ANQ71j8EAAAA&oh=00_AT-pq7qHZ1DNA8aFx7m7XYNkwbJ8ZwiqnkjHIJChOZRbww&oe=6200DC32",
-    caption: "🇺🇸🇺🇸🇺🇸🇺🇸",
-  },
-]
 
 const SecondPage = () => {
+  const [images, setImages] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  const [pagination, setPagination] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`${process.env.GATSBY_SERVER_URL}/integrations/instagram`)
+      .then(res => {
+        console.log("RES", res)
+        return res.json()
+      })
+      .then(data => {
+        console.log("DATA", data)
+        if (data) {
+          setImages(data.data)
+          setPagination(data.paging)
+          setLoading(false)
+        }
+      })
+  }, [])
+  if (isLoading)
+    return (
+      <Layout>
+        <Seo title="DBG's galley" />
+        <h1>Gallery</h1>
+        <h2>LOADING</h2>
+      </Layout>
+    )
+  //! return for when there is no images to display at the moment.
+  if (images)
+    return (
+      <Layout>
+        <Seo title="DBG's galley" />
+        <h1>Gallery</h1>
+        {images &&
+          images.map(image => {
+            return <img src={image.media_url} />
+          })}
+      </Layout>
+    )
   return (
     <Layout>
       <Seo title="DBG's galley" />
       <h1>Gallery</h1>
-      <Gallery images={IMAGES} />,
+      <h2>NOTHING TO BE SHOWING AT THE MOMENT</h2>
     </Layout>
   )
 }
