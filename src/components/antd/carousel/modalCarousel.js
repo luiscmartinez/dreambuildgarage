ae9a06d505b13772977ff2369a718350c62fbd89
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-21 16:32:07 -0700

    fixing useEffect's dependency array vars
    * adding keys prop to elements returned by the map function

--- src/components/antd/carousel/modalCarousel.js
+++ src/components/antd/carousel/modalCarousel.js
@@ -6,13 +6,13 @@ export default function ModalCarousel({ img }) {
   const [isLoading, setLoading] = useState(false)
   const [carouselImages, setCarouselImages] = useState(null)
   const carouselRef = useRef(null)
+  const mediaId = img.id
   useEffect(() => {
     setLoading(true)
     let url = `https://dreambuild.herokuapp.com/api/instagram`
     if (process.env.NODE_ENV === "development") {
       url = process.env.NEXTJS_URL + "/instagram"
     }
-    const mediaId = img.id
     fetch(`${url}/${mediaId}`)
       .then(res => {
         console.log("RES", res)
@@ -23,7 +23,7 @@ export default function ModalCarousel({ img }) {
         setLoading(false)
         console.log("DATA", data)
       })
-  }, [carouselImages])
+  }, [mediaId])
   const btns = () => (
     <div>
       <Button
@@ -40,11 +40,11 @@ export default function ModalCarousel({ img }) {
     <div className="carouselContainer" key={img.id + "passdown"}>
       <AntCarousel ref={carouselRef}>
         {carouselImages &&
-          carouselImages.map(image => {
+          carouselImages.map((image, i) => {
             const { media_type } = image
             if (media_type === "VIDEO") {
               return (
-                <div key={image.id}>
+                <div key={image.id + i}>
                   <video controls className="carouselMedia">
                     <track></track>
                     <source src={image.media_url} />
@@ -54,7 +54,7 @@ export default function ModalCarousel({ img }) {
               )
             }
             return (
-              <div key={image.id}>
+              <div key={image.id + i}>
                 <img
                   src={image.media_url}
                   alt="from DBG's Instagram account"
