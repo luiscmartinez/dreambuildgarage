73f7346ed1d57284def9e316f51b1112ee1433a1
Luis Martinez <mrluismartinezzz@gmail.com>
2022-04-21 19:14:23 -0700

    nagivation logo image query

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import * as React from "react"
-// import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Link } from "react-scroll"
 import HamburgerMenu from "react-hamburger-menu"
 import "../css/navigation.css"
+import "../css/header.css"
 
 const navigations = [
   { id: "about", title: "ABOUT US" },
@@ -13,10 +15,30 @@ const navigations = [
 const Header = () => {
   const [isHamburgerOpen, setHamburger] = React.useState(false)
 
+  const imageData = useStaticQuery(graphql`
+    query LandingCarouselQuerys {
+      allFile(filter: { name: { eq: "dbg_logo-384x384" } }) {
+        edges {
+          node {
+            id
+            childrenImageSharp {
+              gatsbyImageData(width: 60)
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const image = imageData.allFile.edges.map(edge => {
+    return getImage(edge.node.childrenImageSharp[0])
+  })[0]
+
   return (
     <header className="site-navigation">
       <div className="headerContainer">
-        <h1 className="headerTitle">{"styleTitle()"}</h1>
+        <h1 className="headerTitle"></h1>
+        <GatsbyImage image={image} style={{ borderRadius: "30px" }} />
         <div className="hamburgerMenu">
           <HamburgerMenu
             isOpen={isHamburgerOpen}
