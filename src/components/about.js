35204d222a9b03265880ffd3035900ed96882521
Luis Martinez <mrluismartinezzz@gmail.com>
2022-04-26 16:57:31 -0700

    about component recieved strapi data

--- src/components/about.js
+++ src/components/about.js
@@ -1,34 +1,36 @@
 import React from "react"
+import { Link } from "react-scroll"
+import { graphql, useStaticQuery } from "gatsby"
+import "../css/about.css"
 
 export const About = () => {
+  const aboutData = useStaticQuery(graphql`
+    query MyQuery {
+      allStrapiAbout {
+        edges {
+          node {
+            id
+            title
+            description
+          }
+        }
+      }
+    }
+  `)
+
+  const about = aboutData.allStrapiAbout.edges[0].node
+
   return (
-    <div id="about" className="about">
-      <h2>Committed to Quality at Every Step</h2>
-      <p>
-        Got car issues but not exactly sure what it could be? Let us diagnose
-        the problem.
-        <br />
-        When you visit Dream Build Garage, it’s important to us that your
-        experience be as smooth & convenient as possible. To better serve you &
-        your car, we keep our shop fully equipped with the latest factory-grade
-        tools & equipment available. This means we are ASE certified mechanics
-        that can handle everything your car needs, including:
-      </p>
-      <ul className="ul-services">
-        <li>A/C Service & Repairs</li>
-        <li>Engine Repairs</li>
-        <li>Brake Repairs</li>
-        <li>Oil Change Services</li>
-        <li>Check Engine Light Diagnostics</li>
-        <li>Suspension Services & Repairs</li>
-        <li>Clutch Repairs</li>
-      </ul>
-      <p>
-        With decades of experience working on European vehicles, we understands
-        the importance of quality when it comes to service, so we’ll never cut
-        corners or rush a job. You can count on us to work with you through
-        every step of the maintenance process.
-      </p>
+    <div className="site-about-section">
+      <div className="about">
+        <h2>{about.title}</h2>
+        <p>{about.description}</p>
+        <div className="about-btn-wrapper">
+          <Link to="services" smooth={true} className="about-section-btn">
+            Our Services
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
